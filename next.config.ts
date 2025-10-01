f6f9dadc141d0a66dbc88d3b8c769d0dab26bffb
KubukaH <87326201+KubukaH@users.noreply.github.com>
2025-10-01 16:03:13 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        ".prisma/client/index-browser": false,
+      },
+    },
+  },
 };
 
 export default nextConfig;
