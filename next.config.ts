755e0ed3f654cdf9dbb92e727f0fcd077ba047a7
KubukaH <kubukahub@gmail.com>
2025-10-01 20:25:05 +0200

    fixing next-config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /** next configs */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ".prisma/client/index-browser": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
