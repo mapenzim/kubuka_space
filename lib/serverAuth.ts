7edf55c93f02bdeaeef7f715909220b929606709
KubukaH <kubukahub@gmail.com>
2025-10-15 13:55:01 +0200

    added a fix

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -1,7 +1,3 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; // adapt path if exported
-import prisma from "./prisma";
-
 export async function requireRoleServer(req: Request, allowed: string[]) {
   // if using next-auth v4 App Router server route, you can use getServerSession with cookies
   // For a simple example: read token from cookies (or call prisma), but simplest is:
