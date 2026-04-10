507394a423516b21973cf2f02c6b79ce36f69624
mapenzim <mudimbam@outlook.com>
2026-04-10 21:24:50 +0200

    compressed vid for data saving

--- components/home/hero_section.tsx
+++ components/home/hero_section.tsx
@@ -1,20 +1,65 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
 import Fading from "../fade";
 
 const HeroSection = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const [loadVideo, setLoadVideo] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setLoadVideo(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.25 }
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+  
   return (
     <div
       className="relative flex flex-col items-center justify-center w-full min-h-screen"
     >
-      <div className="absolute bg-center bg-cover w-full block overflow-hidden">
-        <video className="w-full h-screen overflow-hidden z-10 aspect-auto bg-cover object-cover" autoPlay muted loop>
-          <source src="/vids/bg-vid.mp4" type="video/mp4" />
+      {/** Background */}
+      <div className="absolute w-full overflow-hidden">
+        <video
+          ref={videoRef}
+          className="w-full h-screen object-cover"
+          autoPlay={loadVideo}
+          muted
+          loop
+          playsInline
+          preload="none"
+          poster="/images/hero.jpg" // 🔥 add this image
+        >
+          {loadVideo && (
+            <>
+            <source src="/vids/bg-vid.webm" type="video/webm" />
+            <source src="/vids/bg-vid.mp4" type="video/mp4" />
+            </>
+          )}
         </video>
       </div>
+
+      {/* Content */}
       <div className="absolute flex flex-col w-full max-w-2xl h-full mx-auto mt-64">
         <Fading delay={0.7} direction="down" fullWidth={null} padding={null}>
           <div className="inline-block items-center space-y-5 sm:px-4 justify-center text-slate-400">
-            <h1 className="text-3xl md:text-[62px] text-center">Kubuka Space PBC</h1>
-            <p className="md:text-xs text-[10px] text-center">Scouting the hidden genius. We can give your business or idea some energy to reach the stratosphere.</p>
+            <h1 className="text-3xl md:text-[62px] text-center">
+              Kubuka Space PBC
+            </h1>
+            <p className="md:text-xs text-[10px] text-center">
+              Unlocking Hidden Potential to Accelerate Business Growth.
+            </p>
           </div>
         </Fading>
       </div>
